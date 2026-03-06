0d235963ca94097803c46271778ad51d3d630525
atul0930976-cpu <atul0930976@gmail.com>
2026-03-06 17:42:04 +0000

    Add configuration settings for Telegram bot

---
+++ config.py
@@ -0,0 +1,5 @@
+OWNER_ID = 8199994609  # Replace with your Telegram user ID
+OFFICIAL_GROUP_LINK = "https://t.me/+K47dLZWv3xA5NTdl"  # Replace with your group link
+AUTHORIZED_PRIVATE_USERS = set()  # Start empty, or preload user IDs
+AUTHORIZED_CHATS = set((-1003694167299,1003332800094)) # Start empty, or preload chat IDs
+ADMIN_IDS = [8199994609]  # Replace with your real Telegram user ID
